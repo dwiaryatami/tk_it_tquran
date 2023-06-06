940a3254920437c01fd9eb3656639d3f663a802a
Dwi Aryatami <130719255+dwiaryatami@users.noreply.github.com>
2023-06-06 19:20:48 +0700

    Delete admin directory

--- admin/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer --> 
-<div class="footer">
-            <div class="container text-center">
-                Copyright &copy; 2022 -<?= $d->nama ?>
-            </div>
-        </div>
-</body>
-</html>
